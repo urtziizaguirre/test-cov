15247ff32ab870e39a23cfebd52d953e94049004
ikortabitarte <60194235+ikortabitarte@users.noreply.github.com>
2022-10-27 18:56:35 +0200

    Update Calculator.php

--- src/Calculator.php
+++ src/Calculator.php
@@ -8,7 +8,6 @@ class Calculator
     }
 
     public function evaluate($num1, $num2){
-        private $mezua2="Hau mezu bat da";
         $func = $this->operator;
         return $this->$func($num1, $num2);
     }
