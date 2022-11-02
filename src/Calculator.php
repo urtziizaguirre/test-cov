1fb67785bc13136bf60b88358a2fb02cd87e9a10
urtziizaguirre <114582931+urtziizaguirre@users.noreply.github.com>
2022-11-02 12:54:12 +0100

    Update Calculator.php

--- src/Calculator.php
+++ src/Calculator.php
@@ -8,6 +8,7 @@ class Calculator
     }
 
     public function evaluate($num1, $num2){
+        private $mezua2="Hau mezu bat da";
         $func = $this->operator;
         return $this->$func($num1, $num2);
     }
